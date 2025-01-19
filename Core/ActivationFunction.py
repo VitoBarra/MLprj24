Average plot and Documentation correct into Core
* Fixing grammar errors
* Fixed scale for plot for avereged metrics
* function to set plot style

Core/ActivationFunction.py
@@ -197,15 +197,15 @@ class Binary(ActivationFunction):
 
     This activation function outputs 0 or 1.
     """
-    def __init__(self, trashold: float = 0.0):
+    def __init__(self, threshold: float = 0.0):
 
         super().__init__()
         self.Name = "Binary"
-        self.Thrashold   = trashold
+        self.Threshold   = threshold
 
     def Calculate(self, z: float|np.array(float)) -> float|np.array(float):
 
-        f = np.vectorize(lambda x: 1 if x>self.Thrashold else 0)
+        f = np.vectorize(lambda x: 1 if x>self.Threshold else 0)
         return f(z)
 
     def CalculateDerivative(self, z: float|np.array(float)) -> float|np.array(float):
@@ -325,7 +325,7 @@ class SoftARGMax(ActivationFunction):
         # Create the Jacobian matrix (diagonal elements)
         jacobian = np.diag(softmax_output) - softmax_outer
 
-        #TODO: This do not work, ih has to be reducted to a 1D array some how
+        #TODO: This do not work, ih has to be reduced to a 1D array some how
         return jacobian
 
 

Core/Metric.py
@@ -8,7 +8,7 @@ class Metric:
     Base class for error functions used in machine learning.
 
     This class provides an interface for calculating the error between predicted
-    and target values. Subclasses must override the `Error` method.
+    and target values. Subclasses must override the `ComputeMetric` method.
     """
 
     Name: str
@@ -21,13 +21,13 @@ class Metric:
 
     def ComputeMetric(self, val: np.ndarray, target: np.ndarray) -> float:
         """
-        Computes the error between predicted values and target values.
+         Computes the error between predicted values and target values.
 
-        :param val: A numpy array of predicted values.
-        :param target: A numpy array of target (ground truth) values.
-        :return: The computed error as a numpy array.
-        :raises NotImplementedError: If the method is not overridden in a subclass.
-        """
+         :param val: A numpy array of predicted values.
+         :param target: A numpy array of target (ground truth) values.
+         :return: The computed error as a float.
+         :raises NotImplementedError: If the method is not overridden in a subclass.
+         """
         raise NotImplementedError("Must override Error method")
 
     def __Call__(self, val: np.ndarray, target: np.ndarray) -> float:
@@ -41,6 +41,7 @@ class MSE(Metric):
     MSE is a commonly used error function that calculates the average of the
     squared differences between predicted and actual values.
     """
+
     Name: str
 
     def __init__(self):
@@ -87,7 +88,7 @@ class RMSE(Metric):
 
 class MEE(Metric):
     """
-    Computes the Mean Euclidian Error (MEE) between predicted and target values.
+    Computes the Mean Euclidean Error (MEE) between predicted and target values.
 
     MEE measures the average magnitude of the errors without considering their direction.
     """
@@ -98,11 +99,11 @@ class MEE(Metric):
 
     def ComputeMetric(self, val: np.ndarray, target: np.ndarray) -> float:
         """
-        Computes the Mean Euclidian Error.
+        Computes the Mean Euclidean Error.
 
         :param val: A numpy array of predicted values.
         :param target: A numpy array of target (ground truth) values.
-        :return: The Mean Euclidian Error as a float.
+        :return: The Mean Euclidean Error as a float.
         """
         if val.shape != target.shape:
             raise ValueError(f"The size of val and target must be the same but instead where {val.shape} and {target.shape}")
@@ -142,18 +143,31 @@ class MAE(Metric):
 
 class Accuracy(Metric):
     """
-        Computes the Accuracy between predicted and target values.
+    Computes the Accuracy between predicted and target values.
 
-        Accuracy measures the percentage of correctly classified samples.
-        """
+    Accuracy measures the percentage of correctly classified samples.
+    """
+
 
     def __init__(self, inter:ActivationFunction = None):
+        """
+        Initializes the Accuracy metric.
+
+        :param inter: Optional activation function to interpret model output (e.g., for probabilities).
+        """
         super().__init__()
         self.Name = "Accuracy"
         self.dataInterpretation = inter
 
 
     def ComputeMetric(self, val: np.ndarray, target: np.ndarray) -> float:
+        """
+        Computes the Accuracy of predictions.
+
+        :param val: A numpy array of predicted values.
+        :param target: A numpy array of target (ground truth) values.
+        :return: The Accuracy as a float between 0 and 1.
+        """
         # If predictions are probabilities, take the class with the highest probability
         if len(val.shape) > 1 and val.shape[1] > 1:
             val = np.argmax(val, axis=1)

Core/MiniBatchGenerator.py
@@ -5,6 +5,12 @@ from .DataSet.DataSet import DataExamples
 
 
 class MiniBatchGenerator:
+    """
+    A class that generates mini-batches of data for training models.
+
+    This generator splits the dataset into smaller batches of size `BatchSize` for each training step.
+    It is ideal for large datasets that do not fit entirely into memory.
+    """
 
     Data: DataExamples
     IsBatchGenerationFinished: bool
@@ -13,6 +19,13 @@ class MiniBatchGenerator:
     LastPosition: int
 
     def __init__(self, data:DataExamples, batchSize:int):
+        """
+        Initializes the MiniBatchGenerator instance.
+
+        :param data: The dataset containing the input data and target labels.
+        :param batchSize: The size of each mini-batch to be generated.
+        :raises ValueError: If the dataset is None, empty, or if BatchSize is not a positive integer.
+        """
         if data is None:
             raise ValueError("DataSet cannot be None")
         if len(data) == 0:
@@ -28,13 +41,14 @@ class MiniBatchGenerator:
 
     def NextBatch(self) -> np.ndarray | None:
         """
-       Generate the next mini-batch of data.
+         Generates the next mini-batch of data.
 
-       :return: Return:
-       - A list containing up to `BatchSize` data points, starting from the current position.
-       - If fewer than `BatchSize` data points remain, the returned list will include all remaining data.
-       - Returns `None` if all data has been processed and no further batches are available.
-       """
+         :return: A tuple containing:
+                  - batch_data: A numpy array containing up to `BatchSize` data points from the dataset.
+                  - batch_target: A numpy array containing the corresponding target labels for the batch.
+                  - If fewer than `BatchSize` data points remain, the batch will contain all remaining data.
+                  - Returns `None, None` if all data has been processed and no further batches are available.
+         """
 
         if self.LastPosition >= len(self.Data) or self.IsBatchGenerationFinished:
             return None , None
@@ -49,9 +63,10 @@ class MiniBatchGenerator:
 
     def Reset(self) -> None:
         """
-        reset the mini-batch generation. Usually called after each epoch.
-        the next NextBatch() will start from the first data point.
-        with BatchSize == 1 (online-training), the data will be shuffled at each epoch.
+        Resets the mini-batch generation process. Typically called after each epoch.
+        The next call to `NextBatch()` will start from the first data point.
+
+        If `BatchSize` is 1 (online training), the data will be shuffled at the start of each epoch.
         """
         self.IsBatchGenerationFinished = False
         self.LastPosition = 0

Core/Optimizer/BackPropagationMomentum.py
@@ -4,20 +4,41 @@ from Core.Optimizer.Optimizer import Optimizer
 
 
 class BackPropagationMomentum(Optimizer):
+    """
+    Implements BackPropagation with momentum for optimizing neural network weights.
+
+    Attributes:
+        loss_function (LossFunction): The loss function used to compute the loss and its derivative.
+        batchSize (int): The size of the mini-batch for gradient computation.
+        eta (float): The learning rate for optimization.
+        lambda_ (float | None): The regularization factor (L2 regularization). Default is None.
+        alpha (float | None): The momentum factor to accelerate convergence. Default is None.
+        decay_rate (float | None): The learning rate decay factor to reduce eta over time. Default is 0.0.
+    """
 
     def __init__(self, loss_function: LossFunction, batchSize:int, eta: float, lambda_: float | None = None,
                  alpha: float | None = None, decay_rate: float | None = 0.0):
         """
-        Initializes the BackPropagation object with a specific loss function.
-
-        :param loss_function: An instance of LossFunction used to compute the loss and its derivative.
-        :param eta: The learning rate.
+                Initializes the BackPropagationMomentum optimizer with the given parameters.
         """
+
         super().__init__(loss_function,batchSize, eta, lambda_, alpha, decay_rate)
 
 
 
     def ApplyMomentum (self, layer: Layer, layer_grad: np.ndarray):
+        """
+        Applies momentum to the gradient for smoother and faster convergence.
+
+        Momentum helps to accelerate convergence by combining the previous gradient
+        (stored in `layer.LastLayer.Gradient`) with the current gradient. It simulates
+        a velocity component that smoothens the optimization path.
+
+        :param layer: The current layer for which to apply momentum.
+        :param layer_grad: The computed gradient for the current layer.
+        :return: The velocity-adjusted gradient with momentum applied.
+        """
+
         # Calculate and apply the momentum
         if layer.LastLayer.Gradient is None:
             first_velocity = 0

Core/Optimizer/Optimizer.py
@@ -4,6 +4,25 @@ from Core.LossFunction import *
 
 
 class Optimizer:
+    """
+    Base class for optimization algorithms in neural networks. This class provides the
+    foundation for implementing various optimization techniques like gradient descent,
+    momentum, and regularization.
+
+    Attributes:
+        loss_function (LossFunction): The loss function used to compute the loss and its derivative.
+        batchSize (int): The size of the mini-batch for gradient computation.
+        eta (float): The learning rate for the optimization process.
+        lambda_ (float | None): The regularization factor (L2 regularization). Default is None.
+        alpha (float | None): The momentum factor for gradient updates. Default is None.
+        decay_rate (float): The learning rate decay factor. Default is 0.0.
+        iteration (int): The current iteration in the optimization process.
+        deltas (np.ndarray): The list of deltas for each layer, used during backpropagation.
+        updates (list): The list of weight updates for each layer.
+        momentum (bool): A flag indicating whether momentum is used in the optimization process.
+        regularization (bool): A flag indicating whether regularization is applied during optimization.
+    """
+
     batchSize :int
 
     deltas: np.ndarray[float]
@@ -43,6 +62,13 @@ class Optimizer:
 
 
     def StartOptimize(self, model: FeedForwardModel, targets: np.ndarray):
+        """
+        Starts the optimization process by iterating through all layers in reverse order
+        and applying the optimization strategy.
+
+        :param model: The feedforward model containing the layers.
+        :param targets: The target values for the optimization.
+        """
         for layer in reversed(model.Layers):
             self.Optimize(layer, targets)
 
@@ -81,6 +107,13 @@ class Optimizer:
         self.updates.append(layer_grad)
 
     def CalculateDelta(self, layer: Layer, target: np.ndarray):
+        """
+        Computes the delta for a layer during backpropagation.
+
+        :param layer: The current layer for which to compute the delta.
+        :param target: The target values for the given inputs.
+        :return: A numpy array containing the deltas for each unit in the layer.
+        """
         deltas = []
 
         #Output Layer
@@ -106,7 +139,7 @@ class Optimizer:
         else:
             prev_delta = self.deltas
 
-            weights = self.PreProcesWeights(layer)
+            weights = self.PreProcessWeights(layer)
             # Transpose the weight matrix to focus on individual unit outputs
 
             all_weight_T = weights.T
@@ -125,9 +158,16 @@ class Optimizer:
         return deltas
 
     def CalculateGradient(self, layer: Layer):
+        """
+        Computes the gradient of the weights for a layer using the deltas.
+
+        :param layer: The layer for which to compute the gradients.
+        :return: The computed gradient of the weights for the layer.
+        """
 
         # Compute gradients using broadcasting
         gradients = layer.LayerInput[:, :, None] * self.deltas[:, None, :]  # Shape: (batch_size, input_dim, output_dim)
+
 
         # Average gradients across the batch
         layer_grad = np.mean(gradients, axis=0)
@@ -144,7 +184,7 @@ class Optimizer:
 
     def UpdateWeights(self, layer: Layer):
         """
-        Function to update the layer's weights.
+        Function to update the layer's weights by applying the computed gradients.
 
         :param layer: The layer to update.
         """
@@ -165,14 +205,11 @@ class Optimizer:
 
     def ApplyRegularization(self, layer: Layer, layer_grad: np.ndarray):
         """
-        Apply L2 regularization to the gradients of a layer's weights.
+        Applies L2 regularization to the gradients of the weights.
 
-        Arguments:
-            layer (Layer): The current layer for which regularization is applied.
-            layer_grad (np.ndarray): The gradient of the weights for the layer.
-
-        Returns:
-            np.ndarray: The updated gradient    with regularization applied.
+        :param layer: The current layer for which regularization is applied.
+        :param layer_grad: The gradient of the weights for the layer.
+        :return: The updated gradient after applying regularization.
         """
         if not self.regularization:
             return layer_grad
@@ -197,10 +234,23 @@ class Optimizer:
 
 
 
-    def PreProcesWeights(self, layer: Layer):
+    def PreProcessWeights(self, layer: Layer):
+        """
+        Preprocesses the weights for the layer before applying momentum.
+
+        :param layer: The current layer for which to preprocess the weights.
+        :return: The preprocessed weights.
+        """
         return layer.WeightToNextLayer
 
     def ApplyMomentum (self, layer: Layer, layer_grad: np.ndarray):
+        """
+        Applies momentum to the gradient during optimization.
+
+        :param layer: The current layer for which to apply momentum.
+        :param layer_grad: The computed gradient of the weights.
+        :return: The updated gradient with momentum applied.
+        """
         if layer.LastLayer.Gradient is None:
             return None
 

Model/MonkModel.py
@@ -32,20 +32,20 @@ class HyperModel_MONK(HyperModel):
         super().__init__( originalDataset)
         self.k = None
         self.val_split = None
-        self.IntepretationMatric = None
+        self.InterpretationMetric = None
 
     def SetSlit(self,val_split, k):
         self.val_split = val_split
         self.k = k
 
-    def GetHyperParamethers(self) ->HyperBag:
+    def GetHyperParameters(self) ->HyperBag:
         hp = HyperBag()
 
         # Optimizer
         hp.AddChosen("BatchSize",[-1,1,32,64,96,128])
         hp.AddRange("eta", 0.001, 0.2, 0.005)
         if MONK_NUM ==3:
-            hp.AddRange("labda", 0.000, 0.01, 0.005)
+            hp.AddRange("lambda", 0.000, 0.01, 0.005)
         hp.AddRange("alpha", 0.5, 0.9, 0.05)
         hp.AddRange("decay", 0.0003, 0.005, 0.0003)
 
@@ -97,7 +97,7 @@ class HyperModel_MONK(HyperModel):
 
     def PreprocessOutput(self,hp, data_set : DataSet):
         if hp["outFun"].Name == "TanH": # TanH
-            data_set.ApplayTranformationOnLabel(np.vectorize(lambda x: -1 if x == 0 else 1 ))
+            data_set.ApplyTransformationOnLabel(np.vectorize(lambda x: -1 if x == 0 else 1 ))
             Interpretation_metric = Accuracy(Sign())
 
         elif hp["outFun"].Name == "Sigmoid": #asigmoid
@@ -108,7 +108,7 @@ class HyperModel_MONK(HyperModel):
             Interpretation_metric = Accuracy()
         else:
             raise ValueError("value unknown")
-        self.IntepretationMatric= Interpretation_metric
+        self.InterpretationMetric= Interpretation_metric
 
 
     def GetModel(self, hp :HyperBag):
@@ -132,11 +132,11 @@ class HyperModel_MONK(HyperModel):
 
 
         if OPTIMIZER == 1:
-            optimizer = BackPropagation(loss,hp["BatchSize"], hp["eta"], hp["labda"], hp["alpha"],hp["decay"])
+            optimizer = BackPropagation(loss,hp["BatchSize"], hp["eta"], hp["lambda"], hp["alpha"],hp["decay"])
         elif OPTIMIZER == 2:
-            optimizer = BackPropagationNesterovMomentum(loss,hp["BatchSize"], hp["eta"], hp["labda"], hp["alpha"],hp["decay"])
+            optimizer = BackPropagationNesterovMomentum(loss,hp["BatchSize"], hp["eta"], hp["lambda"], hp["alpha"],hp["decay"])
         else:
-            optimizer = Adam(loss,hp["BatchSize"], hp["eta"], hp["labda"], hp["alpha"], hp["beta"] , hp["epsilon"] ,hp["decay"])
+            optimizer = Adam(loss,hp["BatchSize"], hp["eta"], hp["lambda"], hp["alpha"], hp["beta"] , hp["epsilon"] ,hp["decay"])
 
         return  optimizer
 
@@ -190,7 +190,7 @@ def ModelSelection( hyperModel:HyperModel_MONK ,NumberOrTrial: int) -> tuple[Mod
 
 
 
-def GeneratePlot_ForMonk(AccuracyMetric, MetricResults, monkDataset, extraname:str= ""):
+def GeneratePlot_ForMonk(AccuracyMetric, MetricResults, monkDataset, extra_name:str= ""):
     MSEmetric = MSE()
 
     lin_model = LogisticRegression()
@@ -231,24 +231,86 @@ def GeneratePlot_ForMonk(AccuracyMetric, MetricResults, monkDataset, extraname:s
         subplotAxes=axes[1])
     # Adjust layout and save the entire figure
     fig.tight_layout()
-    ShowOrSavePlot(f"{MONK_PLOT_PATH}{MONK_NUM}", f"Loss(MSE)-Accuracy{extraname}")
+    ShowOrSavePlot(f"{MONK_PLOT_PATH}{MONK_NUM}", f"Loss(MSE)-Accuracy{extra_name}")
     plt.close(fig)
 
+def GeneratePlotAverage_ForMonk(Results: list[dict], Metrics: list[str], path=f"{MONK_PLOT_PATH}{MONK_NUM}", name: str = f"MONK{MONK_NUM}_MEAN", tag: str = ""):
+    """
+    Generate plot for MONK using the plot for the mean of individual trials.
+
+    :param Results: List of dictionaries. Each dictionary represents an individual trial and contains metrics with respective values.
+    :param Metrics: List of metrics to plot.
+    :param path: The path where the plot will be saved.
+    :param name: The name of the file for the plot.
+    :param tag: Extra information for the file name.
+    """
+    # Organize data by metric name
+    loss_metrics = {metric: [] for metric in Metrics if metric.endswith("loss")}
+    accuracy_metrics = {metric: [] for metric in Metrics if metric.endswith("Accuracy")}
+
+    warm_up_epochs = 5
+    for trial in Results:
+        for metric in Metrics:
+            if metric.endswith("loss") and metric in trial:
+                loss_metrics[metric].append(trial[metric][warm_up_epochs:])
+            elif metric.endswith("Accuracy") and metric in trial:
+                accuracy_metrics[metric].append(trial[metric][warm_up_epochs:])
+
+    # Plot the metrics
+    fig, axes = plt.subplots(1, 2, figsize=(18, 6))
+
+    PlotAverage(
+        metricDict=loss_metrics,
+        limitYRange=None,
+        WarmUpEpochs=warm_up_epochs,
+        title=f"MONK {MONK_NUM} Loss",
+        xlabel="Epochs",
+        ylabel="Loss",
+        subplotAxes=axes[0],
+    )
+
+    PlotAverage(
+        metricDict=accuracy_metrics,
+        limitYRange=None,
+        WarmUpEpochs=warm_up_epochs,
+        title=f"MONK {MONK_NUM} Accuracy",
+        xlabel="Epochs",
+        ylabel="Accuracy",
+        subplotAxes=axes[1]
+    )
+
+    # Adjust layout and save the entire figure
+    fig.tight_layout()
+    ShowOrSavePlot(f"{path}", f"{name}{tag}")
+    plt.close(fig)
+
+
 def GenerateTagNameFromSettings(settings:dict):
+    """
+    Generate a tag name based on the selected optimizer, data preprocessing, and activation function.
+
+    :return: A string tag name describing the model configuration.
+    """
     tagName = ""
 
     if settings["optimizer"] == 1:
-        tagName += "_backprop"
+        tagName += "_BackPropagation"
     elif settings["optimizer"] == 2:
-        tagName += "_nasterov"
+        tagName += "_NesterovMomentum"
     else:
-        tagName += "_adam"
+        tagName += "_ADAM"
 
     return tagName
 
 
 def TrainMonkModel(NumberOrTrial_search:int, NumberOrTrial_mean:int, monk_To_Test=None) -> None:
+    """
+    Train the MONK model using grid search and random search for hyperparameter optimization.
 
+    :param NumberOrTrial_search: Number of trials for hyperparameter search.
+    :param NumberOrTrial_mean: Number of trials to evaluate the model's performance.
+    :param monk_To_Test : It's which monk dataset will be tested.
+    """
     if monk_To_Test is None:
         monk_To_Test = [1, 2, 3]
 
@@ -284,7 +346,7 @@ def TrainMonkModel(NumberOrTrial_search:int, NumberOrTrial_mean:int, monk_To_Tes
             hyperModel.SetSlit(VAL_SPLIT ,KFOLD_NUM)
             monkDataset.SplitTV()
 
-            print(f"Trasining MONK {MONK_NUM}...")
+            print(f"Training MONK {MONK_NUM}...")
             print(f"Run experiment with the following settings: {tagName}")
 
 
@@ -301,7 +363,7 @@ def TrainMonkModel(NumberOrTrial_search:int, NumberOrTrial_mean:int, monk_To_Tes
             totalResult = ValidateSelectedModel(
                 hyperModel_fn,best_hpSel,
                 NumberOrTrial_mean, MetricToCheck,
-                hyperModel.IntepretationMatric
+                hyperModel.InterpretationMetric
                 ,monk_data.Test,monk_data.Training,
                 500,50,42 )
 
@@ -315,16 +377,21 @@ def TrainMonkModel(NumberOrTrial_search:int, NumberOrTrial_mean:int, monk_To_Tes
 
 
 if __name__ == '__main__':
-        monkNumList = [1,2,3]
-        TrainMonkModel(500,50, monkNumList)
+        monkNumList = [3]
+        TrainMonkModel(100,50, monkNumList)
 
         for monk_num in monkNumList:
             jsonFiles = GetAllFileInDir(f"{MONK_RESUTL_PATH}{monk_num}")
             for jsonFile in jsonFiles:
                 data = readJson(jsonFile)
-                PlotAveragedResults(data["metrics"], ["test_loss", "loss", "test_Accuracy", "Accuracy"],
-                                    path =f"{MONK_PLOT_PATH}{monk_num}",
-                                    filename=f"mean_MONK{GenerateTagNameFromSettings(data['settings'])}.png")
 
-            #GeneratePlot(BaselineMetric_Accuracy, best_model.MetricResults, monkDataset,tagName)
+                GeneratePlotAverage_ForMonk(
+                    Results=data["metrics"],
+                    Metrics=["test_loss", "loss", "test_Accuracy", "Accuracy"],
+                    path=f"{MONK_PLOT_PATH}{monk_num}",
+                    tag=GenerateTagNameFromSettings(data['settings'])
+                )
+
+
+            #GeneratePlot_ForMonk(BaselineMetric_Accuracy, best_model.MetricResults, monkDataset,tagName)
 

dataset/ReadDatasetUtil.py
@@ -4,6 +4,16 @@ from Core.DataSet.DataExamples import DataExamples
 from Core.DataSet.DataSet import DataSet
 
 def readMonk(file_path:str) -> DataExamples:
+    """
+    Reads the Monk dataset from a file and returns it as a DataExamples object.
+
+    The dataset contains 8 columns: the first column is the label, the next six are features, and
+    the last is an ID.
+
+    :param file_path: Path to the Monk dataset file.
+    :return: A DataExamples object containing the dataset (data, labels, and IDs).
+    :raises ValueError: If a line doesn't contain exactly 8 elements.
+    """
     data = []
     labels = []
     ids = []
@@ -13,15 +23,14 @@ def readMonk(file_path:str) -> DataExamples:
             if len(parts) != 8:
                 raise ValueError("we want 8 elements in this file.")
 
-            # I primi sei valori come array di dati
+            # The first six values as data array
             data.append([int(x) for x in parts[1:7]])
-            # Il settimo valore come label
+            # The seventh value as label
             labels.append(int(parts[0]))
-            # L'ottavo valore come ID
+            # The eighth value as ID
             ids.append(parts[7])
 
-
-    # Converts list to array NumPy
+    # Converts list to NumPy array
     data = np.array(data)
     labels = np.array(labels)
     labels = labels.reshape(labels.shape[0], 1)
@@ -29,12 +38,16 @@ def readMonk(file_path:str) -> DataExamples:
     de = DataExamples(data, labels, ids)
     return de
 
-
-
-
-
-
 def readCUP(file_path:str) -> DataSet:
+    """
+    Reads the CUP dataset from a file and returns it as a DataSet object.
+
+    The dataset contains 16 columns: the first is the ID, the next 12 are features, and the last three are labels.
+
+    :param file_path: Path to the CUP dataset file.
+    :return: A DataSet object containing the dataset (data, labels, and IDs).
+    :raises ValueError: If a line doesn't contain exactly 16 elements.
+    """
     data = []
     labels = []
     ids = []
@@ -49,22 +62,29 @@ def readCUP(file_path:str) -> DataSet:
             # ID (first value)
             ids.append([int(parts[0])])
 
-            # data (middle value)
+            # Data (middle values)
             data.append(list(map(float, parts[1:13])))
 
-            # Label (last 3 value)
+            # Labels (last 3 values)
             labels.append(list(map(float, parts[13:])))
 
-    # Converti le liste in array NumPy
+    # Convert lists to NumPy arrays
     data = np.array(data)
     labels = np.array(labels)
     ids = np.array(ids)
     return DataSet.FromData(data, labels, ids)
 
-
-
-
 def CreateFakeData(nData:int, xdim :int=1, ydim:int=1) -> DataSet:
+    """
+    Creates a synthetic dataset with random data and binary labels.
+
+    The data is generated using a uniform distribution for the features and random binary labels.
+
+    :param nData: Number of data points to generate.
+    :param xdim: Number of input features (default is 1).
+    :param ydim: Number of output labels (default is 1).
+    :return: A DataSet object containing the synthetic data.
+    """
     x = np.random.uniform(0, 1, (nData,xdim))
     y = np.random.choice([0, 1], (nData, ydim))
     id = np.array(range(x.shape[0]))